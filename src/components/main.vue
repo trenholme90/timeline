99268404d9afa09d151301517090ca4a7f792756
jtrenholme <james.trenholme@jet2.com>
2019-02-04 14:55:57 +0000

    setup

---
+++ src/components/main.vue
@@ -0,0 +1,13 @@
+import HelloWorld from '@/components/Main'
+
+<template>
+  <div class="page-container">
+    <div class="page-container__inner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
---
+++ src/components/timeline.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="timeline">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timeline'
+}
+</script>
