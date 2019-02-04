68f161f99bbfee7b7f3c8cf5b8ef77cdc872252d
jtrenholme <james.trenholme@jet2.com>
2019-02-04 16:25:08 +0000

    created initial data and hooked it up to timeline component

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,13 +1,19 @@
-import HelloWorld from '@/components/Main'
-
 <template>
   <div class="page-container">
-    <div class="page-container__inner"></div>
+    <div class="page-container__inner">
+        <Timeline></Timeline>
+    </div>
+
   </div>
 </template>
 
 <script>
+import Timeline from './Timeline'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    Timeline
+  }
 }
 </script>
